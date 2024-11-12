240d3139e97b56bdb4323c456f424dccc67f130a
bellavarreola <anar5296@colorado.edu>
2024-11-11 21:50:35 -0700

    final push for wa 11

--- js/wa_11.js
+++ js/wa_11.js
@@ -12,27 +12,50 @@ const imageFilenames = [
     "../img/wa11/pic4.jpeg",
     "../img/wa11/pic5.jpeg"
 ];
-
+ console.log(imageFilenames[0]);
 /* Declaring the alternative text for each image file */
 const imageAltText = {
-    "../img/wa11/pic1.jpg": "Authentic mediterranean cuisine in Portugal",
-    "../img/wa11/pic2.jpg": "Inside of unique library in Portugal",
-    "../img/wa11/pic3.jpg": "Trolley car on street in Portugal",
-    "../img/wa11/pic4.jpg": "Cliff view at Cabo de Roca of ocean with lighthouse",
-    "../img/wa11/pic5.jpg": "Pink Street in Lisbon with famous colorful umbrellas",
+    "../img/wa11/pic1.jpeg": "Authentic mediterranean cuisine in Portugal",
+    "../img/wa11/pic2.jpeg": "Inside of unique library in Portugal",
+    "../img/wa11/pic3.jpeg": "Trolley car on street in Portugal",
+    "../img/wa11/pic4.jpeg": "Cliff view at Cabo de Roca of ocean with lighthouse",
+    "../img/wa11/pic5.jpeg": "Pink Street in Lisbon with famous colorful umbrellas",
 };
 
 /* Looping through images */
-for (let i = 0; i < filenames.length; i++) {
+for (let i = 0; i < imageFilenames.length; i++) {
     const newImage = document.createElement('img');
-    const filename = filenames[0].replace("{number}", i);
+    const filename = imageFilenames[i].replace("{number}", i);
 
-    const srcPath = `images/${filename}`;
-    const altText = imageAltText[filename];
+    const srcPath = imageFilenames[i];
+    const altText = imageAltText[srcPath];
 
     newImage.setAttribute('src', srcPath);
     newImage.setAttribute('alt', altText);
+
+
+    newImage.addEventListener('click', () => {
+        displayedImage.src = srcPath;
+        displayedImage.alt = altText;
+    });
     thumbBar.appendChild(newImage);
-}
+};
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    // Check the current class of the button
+    const currentClass = btn.getAttribute('class');
+
+    if (currentClass === 'dark') {
+        // Change to "light" mode
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // Darken overlay
+    } else {
+        // Change to "dark" mode
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'; // Remove overlay darkening
+    }
+});
